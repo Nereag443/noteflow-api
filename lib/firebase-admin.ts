d3591093d31233aa73d8624419bd1b68bdf6f100
Nereag443 <nerea12172005@gmail.com>
2026-05-26 15:42:38 +0200

    Refactorizada inicialicación de admin para usar variables de entorno para las credenciales

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,9 +1,13 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../firebase-service-account.json';
+import { processFetch } from 'next/dist/client/components/router-reducer/fetch-server-response';
 
 if(!admin.apps.length){
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+        credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        }),
     });
 }
 
